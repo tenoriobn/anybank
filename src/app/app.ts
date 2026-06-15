0e64e5ad57e5e43e1fe97d0deeb39c837f517a52
Bruno Tenorio <tenoriobn@gmail.com>
2026-06-15 08:07:54 -0300

    feat: :sparkles: Módulo 4: Comunicando componente pai com output

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { Banner } from './banner/banner';
 import { FormNovaTransacao } from './form-nova-transacao/form-nova-transacao';
+import { Transacao } from './modelos/transacao';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,7 @@ import { FormNovaTransacao } from './form-nova-transacao/form-nova-transacao';
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('anybank');
+  processarTransacao(transacao: Transacao) {
+    console.log(transacao);
+  }
 }
--- src/app/form-nova-transacao/form-nova-transacao.ts
+++ src/app/form-nova-transacao/form-nova-transacao.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TipoTransacao, Transacao } from '../modelos/transacao';
 
 @Component({
   selector: 'app-form-nova-transacao',
@@ -11,9 +12,16 @@ export class FormNovaTransacao {
   tipoTransacao = '';
   valorTransacao = '';
 
+  transacaoCriada = output<Transacao>();
+
   aoSubmeter() {
-    console.log(this.tipoTransacao);
-    console.log(this.valorTransacao);
+    const transacao = new Transacao(
+      this.tipoTransacao as TipoTransacao,
+      Number(this.valorTransacao),
+    );
+
+    this.transacaoCriada.emit(transacao);
+
     this.tipoTransacao = '';
     this.valorTransacao = '';
   }
